aabb2ea1ddfdc448d3d5838d659454414507c684
Yap Hong Kheng <hong.kheng@sonoport.com>
2015-04-27 16:33:43 +0800

    update npm modules. update gulp build. update html and sass base layout setup.

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # feather
-A light-weight and simple, gulp-based HTML and css boilerplate for websites/applications that uses [soundmodels](https://github.com/sonoport/soundmodels).
+A light-weight and simple, gulp-based HTML and css boilerplate for websites/applications that use [soundmodels](https://github.com/sonoport/soundmodels).
 
 
 # Installation
-
-
 
 WIP
--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ var reload = $.browserSync.reload;
 gulp.task('server', function(){
 	$.browserSync({
 		server: {
-			baseDir: "src/"
+			baseDir: "dist/"
 		},
 		port: 9000
 	});
@@ -24,16 +24,31 @@ gulp.task('server', function(){
 // Complie sass into CSS
 gulp.task('sass', function(){
 	return gulp.src('src/sass/main.scss')
-		.pipe($.sass())
-		.pipe(gulp.dest('src/css'))
+		.pipe($.sass({ errLogToConsole: true}))
+		.on('error', $.util.log.bind($.util, 'Sass Erorr'))
+		.pipe($.autoprefixer({browsers: ['last 1 version']}))
+		.pipe(gulp.dest('dist/css'))
 		.pipe(reload({stream: true}));
 });
 
 // jsHint
-gulp.task('jshint', function(){
+gulp.task('jshint', ['jsbeautifier'], function(){
 	return gulp.src(['gulpfile.js', 'src/js/**/*.js', '!src/js/bundle.js'])
 	.pipe($.jshint())
 	.pipe($.jshint.reporter('jshint-stylish'));
+});
+
+// jsbeautify
+gulp.task('jsbeautify', function(){
+	return gulp.src(['src/js/**/*.js'])
+		.pipe($.jsbeautifier({config: '.jsbeautifyrc'}))
+		.pipe(gulp.dest('src/js'));
+});
+
+// copy misc files from src to dist
+gulp.task('copy-files', function(){
+	return gulp.src(['src/**/*.html'])
+		.pipe(gulp.dest('dist'));
 });
 
 // Browserify build
@@ -46,29 +61,52 @@ function bundle(){
 	return bundler.bundle()
 		.on('error', $.util.log.bind($.util, 'Browserify Error'))
 		.pipe($.source('bundle.js'))
-		.pipe(gulp.dest('src/js'))
+		.pipe(gulp.dest('dist/js'))
 		.pipe(reload({stream: true}));
 }
+
+// release build js files
+gulp.task('buildjs', function(){
+
+	var b = $.browserify('./src/js/site.js');
+
+	return b.bundle()
+		.on('error', $.util.log.bind($.util, 'Browserify Error'))
+		.pipe($.source('bundle.js'))
+		.pipe($.buffer())
+		.pipe($.uglify())
+		.pipe(gulp.dest('dist/js/'))
+});
+
+// clean dist folder
+gulp.task('clean:dist', function(cb){
+	return $.del(['dist/'], cb);
+});
 
 // reload
 gulp.task('reload-sass', ['sass'], function(){
 	reload();
 });
-gulp.task('reload-js', ['browserify'],function(){
+gulp.task('reload-js', ['browserify', 'jshint'],function(){
+	reload();
+});
+gulp.task('reload-html', ['copy-files'], function(){
 	reload();
 });
 
 // watch
-gulp.task('watch', ['sass', 'browserify', 'server'], function(){
+gulp.task('watch', ['sass', 'browserify'], function(){
 	gulp.watch('src/sass/**/*.scss', ['reload-sass']);
-	gulp.watch('src/*.html').on('change', reload);
-	gulp.watch('src/js/*.js', ['jshint','reload-js']);
+	gulp.watch('src/**/*.html', ['reload-html']);
+	gulp.watch('src/js/**/*.js', ['reload-js']);
 });
 
 
 /*** tasks to run ***/
 // Default task
-gulp.task('default', ['watch']);
+gulp.task('default', function(cb){
+	$.runSequence('clean:dist', 'watch','copy-files', 'server',cb);
+});
 
-// gulp : for development and livereload
-
+// Release task
+gulp.task('releasebuild');
